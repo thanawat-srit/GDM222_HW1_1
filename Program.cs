2c7fbaffbc69a0f1753ee0c27e4dce150c037146
Nor_Book <thanawat.srit@kmutt.ac.th>
2023-02-02 15:09:57 +0700

    Add Program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿class Program{
+    static void Main(string[] args){
+        double x = double.Parse(Console.ReadLine());
+        Console.WriteLine(f(x));
+    }
+    static double f(double x){
+        double sum = 0;
+        if(Math.Pow(x,2)<= 4){
+            sum = 2;
+        }else if(Math.Pow(x,2)> 4){
+            sum = 1+f(x-4*Math.Abs(x)/x);
+        }
+        return sum;
+    }
+}
